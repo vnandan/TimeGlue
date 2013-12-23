fd5409704ec397fa3626f30f78d49ea3cef53d1c
vnandan <vipnandan@gmail.com>
2013-12-24 03:43:43 +0530

    validation to node model

--- app/Model/node.php
+++ app/Model/node.php
@@ -4,5 +4,23 @@ class Node extends AppModel
 public $name = 'Node';
 public $belongsTo = 'Thread';
 public $hasMany = array('Video','Blog','Picture');
+public $validate = array(
+                'title' => array(
+                        'not_empty'=> array(
+                                    'rule' => 'notEmpty',
+                                    'message' => 'Title cannot be empty'
+                                        ),
+                        'length_check' => array(
+                                        'rule' => array('between',1,75),
+                                        'message' => 'Inappropriate title length'
+                                        )
+                            ),
+                'reference_date' => array(
+                        'is_date' => array(
+                                        'rule' => 'date',
+                                        'message' => 'Invalid date'
+                                        )
+                            )
+                );
 }
 ?>
