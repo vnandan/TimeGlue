a28aee3142477f50ebfd7ff8e7da2a5373f420ec
vnandan <vipnandan@gmail.com>
2013-12-24 19:40:54 +0530

    adding CRUD to threads

--- app/Controller/ThreadsController.php
+++ app/Controller/ThreadsController.php
@@ -1,19 +1,47 @@
 <?php
 class ThreadsController extends AppController{
-    /*public function view()
-    {
-        $links = $this->Thread->find('all');
-        $this->set('links', $links);
+public $helpers = array('Html', 'Form', 'Session');
+
+public function index() {
+        $this->set('threads', $this->Thread->find('all'));
     }
-    
-    public function add()
+
+public function view($id = null)
+{
+     if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+    else
     {
-        if(!empty($this->data))
-        {
-            $this-
+    $this->Thread->recursive=2;
+    $thread = $this->Thread->findById($id);
+        if (!$thread) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+    $this->set('thread',$thread);
+    }
+}
+public function add() {
+        if ($this->request->is('post')) {
+            $this->Thread->create();
+            if ($this->Thread->save($this->request->data)) {
+                $this->Session->setFlash(__('Your Thread has been created.'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
         }
     }
-    */
-    public $scaffold;
+public function delete($id) {
+    if ($this->request->is('get')) {
+        throw new MethodNotAllowedException();
+    }
+
+    if ($this->Thread->delete($id)) {
+        $this->Session->setFlash(
+            __('The post with id: %s has been deleted.', h($id))
+        );
+        return $this->redirect(array('action' => 'index'));
+    }
+}
 }
 ?>
